837685cb4010e418263b9021805c02415cc1b94a
Joe Yu <joeyu23@gmail.com>
2014-04-24 22:53:50 -0700

    Assignment4 uploaded

---
+++ Assignment4/Server.py
@@ -0,0 +1,52 @@
+from network import Listener, Handler, poll
+
+ 
+handlers = {}  # map client handler to user name
+ 
+class MyHandler(Handler):
+     
+    def on_open(self):
+        pass
+         
+    def on_close(self):
+        name = handlers[self]
+        del handlers[self]
+        users = ''
+        for handler in handlers:
+            if users == '':
+                users += handlers[handler]
+            else:
+                users += ', ' + handlers[handler]
+        for handler in handlers:
+            handler.do_send({'leave':name, 'users':users})
+     
+    def on_msg(self, msg):
+        if 'join' in  msg:
+            handlers[self] = msg['join']
+            users = ''
+            for handler in handlers:
+                if users == '':
+                    users += handlers[handler]
+                else:
+                    users += ', ' + handlers[handler]
+            for handler in handlers:
+                handler.do_send({'join':msg['join'], 'users':users})
+        elif 'speak' in msg and msg['txt'] != '':
+            for handler in handlers:
+                if handlers[handler] != msg['speak']:
+                    handler.do_send({'name':msg['speak'], 'txt':msg['txt']})
+ 
+port = 8888
+server = Listener(port, MyHandler)
+while 1:
+    try:
+        poll(timeout=0.05) # in seconds
+    except KeyboardInterrupt:
+        print "Closing server, Bye: "
+        for client in handlers:
+            print client
+            Handler.do_close(handlers[client])
+        print handlers.keys()
+
+        print "Server Shutdown Complete..."
+        sys.exit()
